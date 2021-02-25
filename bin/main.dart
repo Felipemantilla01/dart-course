ac7d0545c1cdce41e3557634dc5178f610bfaa39
Felipe mantilla <felipemantilla@outlook.es>
2021-02-25 05:21:56 -0500

    [feature] add diff comments on the code

--- bin/main.dart
+++ bin/main.dart
@@ -1,27 +1,14 @@
 void main() {
-  //Numeric Data
-  int intVariable = 10;
-  double decimalVariable = 3.1416;
-  print(intVariable);
-  print(decimalVariable);
+  //Comentario de una sola linea
 
-  // Strings
-  String str = "Dart esta interesante";
-  print(str);
+  /**
+   * @params
+   * @kg -> parametro a convertir
+   */
 
-  //booleans
-  bool onOff = true;
-  onOff = false;
+  /* 
 
-  //dynamic data
+   Hola mundo, comentarios en bloque 
 
-  dynamic some = 10.3;
-  some = 3;
-  some = 'String or something else';
-
-  some = decimalVariable;
-  some = intVariable;
-  some = onOff;
-
-  print(' hola $some');
+  */
 }
