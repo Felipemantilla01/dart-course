97b4824e39d5c7e19e4b23397db8b98032e948f0
Felipe mantilla <felipemantilla@outlook.es>
2021-02-28 05:38:11 -0500

    [feature] create code for manipule Maps. :)

--- bin/main.dart
+++ bin/main.dart
@@ -1,42 +1,26 @@
 void main(List<String> args) {
-  List<int> intList = [1, 2, 4, 6, 12];
+  Map<String, int> mapStringInt = {'tres': 3, 'dos': 2, 'cien': 100};
 
-  print(intList);
+  Map<String, String> mapStringString = {
+    'hola': 'mundo',
+    'soy': 'Felipe',
+    'Mantilla': 'Gomez'
+  };
 
-  intList.forEach((element) {
-    print(element);
+  print(mapStringString['soy']);
+  print(mapStringInt['cien']);
+
+  mapStringString.forEach((key, value) {
+    print('$key : $value');
   });
 
-  for (var i in intList) {
-    print(i);
-  }
+  mapStringString.update('Mantilla', (value) => "Guevara");
 
-  print('hola : ${intList[2]}');
-  print('${intList}');
+  mapStringString.remove('hola');
 
-  intList.add(3);
+  print(mapStringString);
 
-  intList.addAll([3, 6, 2]);
+  mapStringString.removeWhere((key, value) => key == 'soy');
 
-  intList.insert(3, 55);
-
-  print(intList);
-
-  intList.remove(3);
-
-  print(intList);
-
-  intList.removeAt(0);
-
-  print(intList);
-
-  intList.removeWhere((element) => element == 55);
-
-  print(intList);
-
-  print(intList.asMap());
-
-  List menoresDef5 = intList.where((element) => element < 5).toList();
-
-  print(menoresDef5);
+  print(mapStringString);
 }
