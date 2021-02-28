45586fdac5dbd1e22b3c179984b9c200d60bb8da
Felipe mantilla <felipemantilla@outlook.es>
2021-02-28 07:05:51 -0500

    [feature] understanding closures

--- bin/main.dart
+++ bin/main.dart
@@ -1,17 +1,8 @@
+Function makeAdder(num addBy) {
+  return (num i) => addBy + i;
+}
+
 void main(List<String> args) {
-  bool level1 = true;
-
-  if (level1) {
-    bool level2 = true;
-    if (level2) {
-      bool level3 = false;
-      if (level3) {
-        print(level1);
-        print(level2);
-        print(level3);
-      }
-    }
-  }
-
-  //print(level3) //note this error
+  var add2 = makeAdder(2);
+  print(add2(4));
 }
