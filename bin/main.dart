3b09d87499863fa618ddb400951a3fdc85dc4dc1
Felipe mantilla <felipemantilla@outlook.es>
2021-02-28 06:51:37 -0500

    [feature] using unamed functions

--- bin/main.dart
+++ bin/main.dart
@@ -1,14 +1,7 @@
-import 'package:meta/meta.dart';
-
 void main(List<String> args) {
-  var result = resta(a: 2, b: suma(a: 4, b: 3));
-  print(result);
-}
+  var list = ['rojo', 'azul', 'amarillo'];
 
-int suma({@required int a, @required int b}) {
-  return a + b;
-}
-
-int resta({@required int a, @required int b}) {
-  return a - b;
+  list.forEach((color) {
+    print(color);
+  });
 }
