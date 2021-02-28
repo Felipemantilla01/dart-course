654a09fdfbcc546f0bed875ef987911a6706cc61
Felipe mantilla <felipemantilla@outlook.es>
2021-02-28 06:11:04 -0500

    [feature] using use else with relational operators

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,11 @@
 void main(List<String> args) {
-  List<int> listInt = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  bool acceso = true;
+  int costo = 10;
+  String cupon = 'gratis';
 
-  listInt
-    ..clear()
-    ..add(10)
-    ..addAll([15, 2])
-    ..remove(2)
-    ..removeWhere((element) => element == 15);
-
-  print(listInt);
+  if ((acceso && costo >= 20) || cupon == 'gratis') {
+    print('Bienvenido al curso de dart');
+  } else {
+    print('no tienes acceso');
+  }
 }
