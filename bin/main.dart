8a5c45facdc7e69c8eb00c8807f82160dc8593d5
Felipe mantilla <felipemantilla@outlook.es>
2021-02-28 06:49:35 -0500

    [feature] using function as a parameter for other function

--- bin/main.dart
+++ bin/main.dart
@@ -1,27 +1,14 @@
 import 'package:meta/meta.dart';
 
 void main(List<String> args) {
-  String result = holaMundo();
+  var result = resta(a: 2, b: suma(a: 4, b: 3));
   print(result);
-
-  int sum = sumar(3, 5);
-  print(sum);
-
-  String option = optionalParameters();
 }
 
-String holaMundo() {
-  String hola = 'Hola mundo';
-  return hola;
+int suma({@required int a, @required int b}) {
+  return a + b;
 }
 
-int sumar(int a, int b) {
-  return (a + b);
-}
-
-String optionalParameters({int number, @required String option}) {
-  number ??= 30; // this is used for assign a value in case null / undefined
-  print(number);
-  print(option);
-  return 'Hola mundo ';
+int resta({@required int a, @required int b}) {
+  return a - b;
 }
