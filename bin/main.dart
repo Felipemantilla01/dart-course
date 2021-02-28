b0ee25a61bdd4298ebbbfb3296e65116564dd993
Felipe mantilla <felipemantilla@outlook.es>
2021-02-28 05:44:30 -0500

    [feature] create code for arithmetic operators

--- bin/main.dart
+++ bin/main.dart
@@ -1,26 +1,15 @@
 void main(List<String> args) {
-  Map<String, int> mapStringInt = {'tres': 3, 'dos': 2, 'cien': 100};
+  double celcius, farenheit = 54;
 
-  Map<String, String> mapStringString = {
-    'hola': 'mundo',
-    'soy': 'Felipe',
-    'Mantilla': 'Gomez'
-  };
+  int a = 10, b = 3, residuo;
 
-  print(mapStringString['soy']);
-  print(mapStringInt['cien']);
+  celcius = (farenheit - 32) / 1.8;
+  print('grados celcius : $celcius');
+  celcius++;
+  print('grados celcius : $celcius');
+  celcius += 10;
+  print('grados celcius : $celcius');
 
-  mapStringString.forEach((key, value) {
-    print('$key : $value');
-  });
-
-  mapStringString.update('Mantilla', (value) => "Guevara");
-
-  mapStringString.remove('hola');
-
-  print(mapStringString);
-
-  mapStringString.removeWhere((key, value) => key == 'soy');
-
-  print(mapStringString);
+  residuo = a % b;
+  print('residuo : $residuo');
 }
