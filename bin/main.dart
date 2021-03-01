f51ece965828d972a02f328c1e4dbeab49ff6449
Felipe mantilla <felipemantilla@outlook.es>
2021-03-01 05:40:38 -0500

    [feature] user Compliter class and use try catch to handle errors

--- bin/main.dart
+++ bin/main.dart
@@ -1,24 +1,30 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:test/test.dart';
 
 void main(List<String> args) async {
   print('Elaborando peticion');
-  print('Status code : ${await getStatusCode()}');
+  try {
+    print('Status code : ${await getStatusCode()}');
+  } catch (e) {
+    print(e);
+  }
 }
 
-Future<int> getStatusCode() async {
-  http.Response response =
-      await http.get('https://jsonplaceholder.typicode.com/users/1');
+Future<String> getStatusCode() async {
+  Completer completer = new Completer<String>();
 
-  if (response.statusCode == 200) {
-    final jsonData = jsonDecode(response.body);
-    print(jsonData);
-    Map<String, dynamic> mapDatos = jsonData;
-    print(mapDatos);
-    print('Peticion elaborada correctamente');
-    return response.statusCode;
-  } else {
-    return response.statusCode;
+  try {
+    http.Response response =
+        await http.get('https://jsonplaceholder.typicode.com/useras/1');
+    if (response.statusCode == 200) {
+      completer.complete('Peticion http realizada correctamente');
+    } else {
+      completer.completeError('Error en la peticion http');
+    }
+  } catch (e) {
+    completer.completeError('Error de conexion');
   }
+  return completer.future;
 }
