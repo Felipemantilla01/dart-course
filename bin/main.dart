0182627e5e9f02cabe117f9fdb18d44400229f7a
Felipe mantilla <felipemantilla@outlook.es>
2021-03-01 03:51:35 -0500

    [feature] understanding methods and abstract classes

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,43 @@
-import 'triangulo.dart';
-
 void main(List<String> args) {
-  Triangulo triangulo = new Triangulo();
-  triangulo.base = 10;
-  triangulo.altura = 20;
+  //Automovil auto = new Automovil(); // esto no es posible porque la clase es abstracta;
 
-  print('Area ${triangulo.area} ');
+  Carro carro = new Carro();
+  carro.cilindros = 10;
+  carro.velocidad();
+
+  Trailer trailer = new Trailer();
+  trailer.cilindros = 3;
+  trailer.velocidad();
+}
+
+abstract class Automovil {
+  int cilindros;
+  int llantas;
+  void velocidad();
+}
+
+class Carro implements Automovil {
+  @override
+  int cilindros;
+
+  @override
+  int llantas;
+
+  @override
+  void velocidad() {
+    print('200 km/h');
+  }
+}
+
+class Trailer implements Automovil {
+  @override
+  int cilindros;
+
+  @override
+  int llantas;
+
+  @override
+  void velocidad() {
+    print('300 km/h');
+  }
 }
