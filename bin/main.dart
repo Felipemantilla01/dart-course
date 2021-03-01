6361c99a71eefb2076194c8b8bf0cca4fdde289b
Felipe mantilla <felipemantilla@outlook.es>
2021-03-01 04:17:31 -0500

    [feature] understanding mixins.

--- bin/main.dart
+++ bin/main.dart
@@ -1,32 +1,49 @@
+abstract class Animal {
+  animal() => print('Soy un animal');
+}
+
+abstract class Mamimero extends Animal {
+  mamimero() => print('Soy un mamifero');
+}
+
+abstract class Ave extends Animal {
+  ave() => print('Soy un Ave');
+}
+
+abstract class Pez extends Animal {
+  pez() => print('Soy un pez');
+}
+
+abstract class Caminar {
+  caminar() => print('Puedo caminar');
+}
+
+abstract class Nadar {
+  nadar() => print('Puedo Nadar');
+}
+
+abstract class Volar {
+  volar() => print('Puedo volar');
+}
+
+class Murcielago extends Mamimero with Caminar, Volar {
+  murcielago() => print('soy un murcielago');
+}
+
+class Pato extends Ave with Caminar, Volar, Nadar {
+  pato() => print('soy un pato');
+}
+
+class Tiburon extends Pez with Nadar {
+  tiburon() => print('soy un tiburon');
+}
+
 void main(List<String> args) {
-  //Automovil auto = new Automovil(); // esto no es posible porque la clase es abstracta;
-
-  Carro carro = new Carro();
-  carro.turbo = true;
-  carro.cilindros = 10;
-  carro.velocidad('200km/h');
-  carro.infoCarro();
-}
-
-class Automovil {
-  int cilindros;
-  int llantas;
-  int puertas;
-  String combustible;
-  void velocidad(String velocidad) {
-    print('Velocidad $velocidad');
-  }
-}
-
-class Carro extends Automovil {
-  bool turbo;
-  void infoCarro() {
-    print('Turbo $turbo');
-  }
-
-  @override
-  void velocidad(String velocidad) {
-    super.velocidad(velocidad);
-    print('Sobreescritura de metodos');
-  }
+  Murcielago murcielago = new Murcielago();
+  murcielago
+    ..murcielago()
+    ..animal()
+    ..mamimero()
+    ..volar()
+    ..caminar();
 }
