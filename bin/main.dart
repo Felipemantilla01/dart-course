00fb3b4e0bffce6f8d3a8a2568e7d76345b35f3f
Felipe mantilla <felipemantilla@outlook.es>
2021-03-01 05:31:56 -0500

    [feature] install http dependencie, use jsonDecode to parse http body, use async await and Future

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,24 @@
-import 'package:hello_world/gerente.dart';
-import 'package:hello_world/supervisor.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:test/test.dart';
 
-void main(List<String> args) {
-  // Gerente gerente = new Gerente();
-  Supervisor supervisor =
-      new Supervisor(nombre: 'Felipe', area: 'Mantenimiento');
-  print('Supervisor: ${supervisor.nombre}');
+void main(List<String> args) async {
+  print('Elaborando peticion');
+  print('Status code : ${await getStatusCode()}');
+}
 
-  supervisor.codigoEmpleado(4521);
+Future<int> getStatusCode() async {
+  http.Response response =
+      await http.get('https://jsonplaceholder.typicode.com/users/1');
 
-  Gerente gerente = new Gerente(area: 'MTTO', nombre: 'Roger');
-
-  gerente.codigoEmpleado('GTE');
+  if (response.statusCode == 200) {
+    final jsonData = jsonDecode(response.body);
+    print(jsonData);
+    Map<String, dynamic> mapDatos = jsonData;
+    print(mapDatos);
+    print('Peticion elaborada correctamente');
+    return response.statusCode;
+  } else {
+    return response.statusCode;
+  }
 }
