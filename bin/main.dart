903519108e004a948e9e533895335ba85f94a835
Felipe mantilla <felipemantilla@outlook.es>
2021-03-01 03:35:30 -0500

    [feature] using a factory constructor

--- bin/main.dart
+++ bin/main.dart
@@ -1,43 +1,18 @@
 void main(List<String> args) {
-  // 1. Uso de un constructor normal
-  //  Animales animal = new Animales('Leon', 'Carnivoro');
+  var datos = {'nombre': 'Felipe', 'apellido': 'Mantilla'};
 
-  // 2. Uso de un constructor nombrado
-  Animales animal = new Animales(nombre: 'Leon', tipo: 'Carnivoro');
+  Persona persona = new Persona.asignarDatos(datos);
 
-  // 3. Uso de un constructor particular
-  // var animal = Animales.fromJson({'nombre': 'serpiente', 'tipo': 'Hervivoro'});
-
-  // 4. redireccion de constructores
-  Animales animal2 = Animales.tipoAnimal('Carnivoro');
-
-  print('Animal ${animal.nombre} - ${animal.tipo} ');
-  print('Animal ${animal2.nombre} - ${animal2.tipo} ');
+  print('Persona ${persona.nombre} - ${persona.apellido}');
 }
 
-class Animales {
+class Persona {
   String nombre;
-  String tipo;
+  String apellido;
 
-  // 1.  este es el constructor y debe tener el mismo nombre de la clase
-  // Animales(String nombre, String tipo) {
-  //   this.nombre = nombre;
-  //   this.tipo = tipo;
-  // }
+  Persona({this.nombre, this.apellido});
 
-  // 2. este es el mismo constructor pero con parametros nombrados
-  Animales({this.nombre, this.tipo});
-
-  // 3.  este es otro constructor, uno particular
-  Animales.fromJson(Map Json) {
-    nombre = Json['nombre'];
-    tipo = Json['tipo'];
+  factory Persona.asignarDatos(Map jsonMap) {
+    return Persona(nombre: jsonMap['nombre'], apellido: jsonMap['apellido']);
   }
-
-  // 4. redireccion de constructores,
-  // esto al ser llamado, ejecuta el constructor original, pasandole el tipoAnimal
-  // y le asigna el nombre Tigre por defecto.
-
-  Animales.tipoAnimal(String tipoAnimal)
-      : this(nombre: 'Tigre', tipo: tipoAnimal);
 }
